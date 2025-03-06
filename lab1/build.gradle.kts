bedf028a527757d3f53b289f0d35e1066f9704aa
Adi <adrian.bubuianu@gmail.com>
2025-03-06 14:41:04 +0200

    lab2

--- lab1/build.gradle.kts
+++ lab1/build.gradle.kts
@@ -1,5 +1,21 @@
 plugins {
     id("java")
+    id("application")
+}
+
+application {
+    mainClass = "org.example.Main"
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"]= "org.example.Main"
+    }
+    from({
+        configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+    })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
 }
 
 group = "org.example"
